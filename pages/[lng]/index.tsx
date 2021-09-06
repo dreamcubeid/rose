75cffe26cb8787f458b839f8224527eb1b0d0c21
naimnilamsyahrin <naimnilamsyahrin@gmail.com>
2021-09-07 00:33:04 +0700

    feat: banner component, header fix, instafeed, products homepage, widget

--- pages/[lng]/index.tsx
+++ pages/[lng]/index.tsx
@@ -2,71 +2,54 @@
 import { FC, useState } from 'react'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { useRouter } from 'next/router'
-import Carousel from '@brainhubeu/react-carousel'
 import { LazyLoadComponent } from 'react-lazy-load-image-component'
 import {
-  Banner,
   getBanner,
   Products,
-  ProductCategory,
   useI18n,
   Widget
 } from '@sirclo/nexus'
 /* library template */
 import useWindowSize from 'lib/useWindowSize'
 import { parseCookies } from 'lib/parseCookies'
-import { useSizeBanner } from 'lib/useSizeBanner'
 import { GRAPHQL_URI } from 'lib/Constants'
 import { useBrand } from 'lib/useBrand'
 /* components */
 import Layout from 'components/Layout/Layout'
+import BannerComponent from 'components/Banner'
+import Instafeed from 'components/Instafeed'
 import Placeholder from 'components/Placeholder'
 /* styles */
-import styles from 'public/scss/pages/Home.module.scss'
-
-const classesBanner = {
-  imageContainerClassName: styles.bannerCarousel_header,
-  linkClassName: styles.bannerCarousel_link,
-  imageClassName: styles.bannerCarousel_image
-}
+import styleBanner from 'public/scss/components/Banner.module.scss'
+import styleWidget from 'public/scss/components/Widget.module.scss'
+import styleProduct from 'public/scss/components/Product.module.scss'
+import styleButton from 'public/scss/components/Button.module.scss'
+import stylePlaceholder from 'public/scss/components/Placeholder.module.scss'
 
 const classesProducts = {
-  productContainerClassName: `col-6 col-md-3 product_list ${styles.product}`,
-  stickerContainerClassName: styles.product_sticker,
-  outOfStockLabelClassName: `${styles.product_stickerLabel} ${styles.product_stickerLabel__outofstock}`,
-  saleLabelClassName: `${styles.product_stickerLabel} ${styles.product_stickerLabel__sale}`,
-  comingSoonLabelClassName: `${styles.product_stickerLabel} ${styles.product_stickerLabel__comingsoon}`,
-  openOrderLabelClassName: `${styles.product_stickerLabel} ${styles.product_stickerLabel__openorder}`,
-  preOrderLabelClassName: `${styles.product_stickerLabel} ${styles.product_stickerLabel__preorder}`,
-  newLabelClassName: `${styles.product_stickerLabel} ${styles.product_stickerLabel__new}`,
-  productImageContainerClassName: styles.product_link,
-  productImageClassName: styles.product_link__image,
-  productLabelContainerClassName: styles.product_label,
-  productTitleClassName: styles.product_label__title,
-  productPriceClassName: styles.product_labelPrice,
-  salePriceClassName: styles.product_labelPrice__sale,
-  priceClassName: styles.product_labelPrice__price
-}
-
-const classesProductCategory = {
-  parentCategoryClassName: styles.category_order,
-  categoryItemClassName: styles.category_list,
-  categoryValueClassName: styles.category_list_link,
-  categoryNameClassName: styles.category_list_item,
-  categoryNumberClassName: "ml-1",
-  dropdownIconClassName: "d-none"
-}
-
-const classesPlaceholderBanner = {
-  placeholderImage: `${styles.placeholderItem} ${styles.placeholderItem__banner}`
+  productContainerClassName: styleProduct.product_item,
+  stickerContainerClassName: styleProduct.product_sticker,
+  outOfStockLabelClassName: `${styleProduct.product_stickerLabel} ${styleProduct.product_stickerLabel__outofstock}`,
+  saleLabelClassName: `${styleProduct.product_stickerLabel} ${styleProduct.product_stickerLabel__sale}`,
+  comingSoonLabelClassName: `${styleProduct.product_stickerLabel} ${styleProduct.product_stickerLabel__comingsoon}`,
+  openOrderLabelClassName: `${styleProduct.product_stickerLabel} ${styleProduct.product_stickerLabel__openorder}`,
+  preOrderLabelClassName: `${styleProduct.product_stickerLabel} ${styleProduct.product_stickerLabel__preorder}`,
+  newLabelClassName: `${styleProduct.product_stickerLabel} ${styleProduct.product_stickerLabel__new}`,
+  productImageContainerClassName: styleProduct.product_link,
+  productImageClassName: styleProduct.product_link__image,
+  productLabelContainerClassName: styleProduct.product_label,
+  productTitleClassName: styleProduct.product_label__title,
+  productPriceClassName: styleProduct.product_labelPrice,
+  salePriceClassName: styleProduct.product_labelPrice__sale,
+  priceClassName: styleProduct.product_labelPrice__price
 }
 
 const classesPlaceholderProduct = {
-  placeholderImage: `${styles.placeholderItem} ${styles.placeholderItem_product__card}`,
+  placeholderImage: `${stylePlaceholder.placeholderItem} ${stylePlaceholder.placeholderItem_productCard}`,
 }
 
-const classesPlaceholderCatProduct = {
-  placeholderTitle: `${styles.placeholderItem} ${styles.placeholderItem_productCat__title}`,
+const classesPlaceholderWidgetService = {
+  placeholderImage: `${stylePlaceholder.placeholderItem} ${stylePlaceholder.placeholderItem_widgetService}`,
 }
 
 const Home: FC<any> = ({
@@ -78,7 +61,6 @@ const Home: FC<any> = ({
   const i18n: any = useI18n()
   const router: any = useRouter()
   const size = useWindowSize()
-  const [totalItemFeatured, setTotalItemFeatured] = useState(null)
   const [totalItem, setTotalItem] = useState(null)
 
   return (
@@ -87,201 +69,99 @@ const Home: FC<any> = ({
       lng={lng}
       lngDict={lngDict}
       brand={brand}
+      withAnnouncement={true}
+      headerTitle="Page"
     >
-      <div className={styles.bannerCarousel}>
-        <Banner
+      <div className={styleBanner.bannerCarousel}>
+        <BannerComponent
           data={dataBanners?.data}
-          Carousel={Carousel}
-          classes={classesBanner}
-          autoPlay={5000}
-          infinite
-          thumborSetting={{
-            width: useSizeBanner(size.width),
-            format: "webp",
-            quality: 85,
-          }}
-          loadingComponent={
-            <Placeholder classes={classesPlaceholderBanner} withImage />
-          }
-          widthImage={size.width}
-          lazyLoadedImage={false}
+          size={size}
         />
       </div>
-      <div className={styles.category}>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-lg-8 offset-lg-2">
-              <ProductCategory
-                classes={classesProductCategory}
-                showCategoryNumber={false}
-                loadingComponent={
-                  <div className="container">
+      <div className={styleWidget.widget_services}>
+        <Widget
+          pos="main-content-1"
+          containerClassName={styleWidget.widget_servicesContainer}
+          widgetClassName={styleWidget.widget_servicesItem}
+          loadingComponent={
+            <Placeholder classes={classesPlaceholderWidgetService} withImage />
+          }
+          thumborSetting={{
+            width: size.width < 768 ? 300 : 500,
+            format: "webp",
+            quality: 85
+          }}
+        />
+      </div>
+      {(totalItem > 0 || totalItem === null) &&
+        <div className={styleProduct.product_container}>
+          <div className="container">
+            <div className={styleProduct.product_containerItem}>
+              <LazyLoadComponent>
+                <Products
+                  itemPerPage={8}
+                  classes={classesProducts}
+                  pathPrefix="product"
+                  lazyLoadedImage={false}
+                  getPageInfo={(pageInfo) => setTotalItem(pageInfo.totalItems)}
+                  thumborSetting={{
+                    width: size.width < 768 ? 512 : 800,
+                    format: "webp",
+                    quality: 85
+                  }}
+                  loadingComponent={
                     <div className="row">
-                      <div className="col-4 col-md-2">
-                        <Placeholder classes={classesPlaceholderCatProduct} withTitle />
+                      <div className="col-6">
+                        <Placeholder classes={classesPlaceholderProduct} withImage />
                       </div>
-                      <div className="col-4 col-md-2">
-                        <Placeholder classes={classesPlaceholderCatProduct} withTitle />
-                      </div>
-                      <div className="col-4 col-md-2">
-                        <Placeholder classes={classesPlaceholderCatProduct} withTitle />
-                      </div>
-                      <div className="d-none d-md-block col-md-2">
-                        <Placeholder classes={classesPlaceholderCatProduct} withTitle />
-                      </div>
-                      <div className="d-none d-md-block col-md-2">
-                        <Placeholder classes={classesPlaceholderCatProduct} withTitle />
-                      </div>
-                      <div className="d-none d-md-block col-md-2">
-                        <Placeholder classes={classesPlaceholderCatProduct} withTitle />
+                      <div className="col-6">
+                        <Placeholder classes={classesPlaceholderProduct} withImage />
                       </div>
                     </div>
-                  </div>
-                }
-              />
+                  }
+                />
+              </LazyLoadComponent>
+            </div>
+            <div className="text-center mt-4">
+              <button
+                className={`${styleButton.btn} ${styleButton.btn_secondary}`}
+                onClick={() => router.push(`/${lng}/products`)}
+              >
+                {i18n.t("product.seeAll")}
+              </button>
             </div>
           </div>
         </div>
-      </div>
-      {(totalItemFeatured > 0 || totalItemFeatured === null) &&
-        <section>
-          <div className="container">
-            <div className="row">
-              <div className="col-12 col-lg-8 offset-lg-2">
-                <h5>{i18n.t("product.featuredProducts")}</h5>
-                <div className="row mt-4">
-                  <LazyLoadComponent>
-                    <Products
-                      filter={{ openOrderScheduled: false, published: true }}
-                      tagName="featured"
-                      itemPerPage={8}
-                      classes={classesProducts}
-                      getPageInfo={(pageInfo) => setTotalItemFeatured(pageInfo.totalItems)}
-                      pathPrefix="product"
-                      lazyLoadedImage={false}
-                      thumborSetting={{
-                        width: size.width < 768 ? 512 : 800,
-                        format: "webp",
-                        quality: 85
-                      }}
-                      loadingComponent={
-                        <>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                        </>
-                      }
-                    />
-                  </LazyLoadComponent>
-                </div>
-                {totalItemFeatured > 8 &&
-                  <div className="text-center mt-4">
-                    <a
-                      className={`btn ${styles.btn_secondary} ${styles.btn_long}`}
-                      onClick={() => router.push(`/${lng}/products?tagname=featured`)}
-                    >
-                      {i18n.t("product.seeAll")}
-                    </a>
-                  </div>
-                }
-              </div>
-            </div>
-          </div>
-        </section>
       }
-      <section>
+      <div className="container">
+        <Widget
+          pos="main-content-2"
+          containerClassName={styleWidget.widget_banner}
+          widgetClassName={styleWidget.widget_bannerItem}
+          loadingComponent={
+            <>
+              <div className="col-12">
+                <Placeholder classes={classesPlaceholderProduct} withImage />
+              </div>
+            </>
+          }
+          thumborSetting={{
+            width: size.width < 768 ? 576 : 900,
+            format: "webp",
+            quality: 85
+          }}
+        />
+      </div>
+      {brand?.socmedSetting?.instagramToken &&
         <div className="container">
-          <div className="row">
-            <div className="col-12 col-lg-8 offset-lg-2">
-              <div className="row">
-                <LazyLoadComponent>
-                  <Widget
-                    pos="main-content-1"
-                    containerClassName={styles.widgetHomePage}
-                    widgetClassName={styles.widgetItemHomePage}
-                    loadingComponent={
-                      <>
-                        <div className="col-6">
-                          <Placeholder classes={classesPlaceholderProduct} withImage />
-                        </div>
-                        <div className="col-6">
-                          <Placeholder classes={classesPlaceholderProduct} withImage />
-                        </div>
-                      </>
-                    }
-                    thumborSetting={{
-                      width: size.width < 768 ? 576 : 1200,
-                      format: "webp",
-                      quality: 85
-                    }}
-                  />
-                </LazyLoadComponent>
-              </div>
-            </div>
-          </div>
+          <LazyLoadComponent>
+            <Instafeed
+              i18n={i18n}
+              brand={brand}
+              withFollowButton
+            />
+          </LazyLoadComponent>
         </div>
-      </section>
-      {(totalItem > 0 || totalItem === null) &&
-        <section>
-          <div className="container">
-            <div className="row">
-              <div className="col-12 col-lg-8 offset-lg-2">
-                <h5>{i18n.t("product.all")}</h5>
-                <div className="row mt-4">
-                  <LazyLoadComponent>
-                    <Products
-                      itemPerPage={8}
-                      classes={classesProducts}
-                      pathPrefix="product"
-                      lazyLoadedImage={false}
-                      getPageInfo={(pageInfo) => setTotalItem(pageInfo.totalItems)}
-                      thumborSetting={{
-                        width: size.width < 768 ? 512 : 800,
-                        format: "webp",
-                        quality: 85
-                      }}
-                      loadingComponent={
-                        <>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                          <div className="col-6 col-md-3">
-                            <Placeholder classes={classesPlaceholderProduct} withImage />
-                          </div>
-                        </>
-                      }
-                    />
-                  </LazyLoadComponent>
-                </div>
-                {totalItem > 8 &&
-                  <div className="text-center mt-4">
-                    <a
-                      className={`btn ${styles.btn_secondary} ${styles.btn_long}`}
-                      onClick={() => router.push(`/${lng}/products`)}
-                    >
-                      {i18n.t("product.seeAll")}
-                    </a>
-                  </div>
-                }
-              </div>
-            </div>
-          </div>
-        </section>
       }
     </Layout >
   )

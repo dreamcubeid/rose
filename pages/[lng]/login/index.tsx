6920e612bfe473c14464fcd007d179b3a00323b5
jetly <jetly27sandita@gmail.com>
2021-09-12 11:35:23 +0700

    fix: prettier
    - from tabs to space

--- pages/[lng]/login/index.tsx
+++ pages/[lng]/login/index.tsx
@@ -21,87 +21,87 @@ import styleForm from 'public/scss/components/Form.module.scss'
 import styleButton from 'public/scss/components/Button.module.scss'
 
 const loginClasses = {
-	containerClassName: styleLogin.login_containerForm,
-	inputContainerClassName: `${styleLogin.login_inputContainer} ${styleForm.form}`,
-	passwordContainerClassName: `${styleLogin.login_passwordContainer}`,
-	buttonClassName: `${styleButton.btn} ${styleButton.btn_primary}`,
-	signUpText: `${styleButton.btn} ${styleButton.btn_secondary} ${styleLogin.login_signUp}`,
-	forgotPasswordClass: styleLogin.login_forgotContainer,
+  containerClassName: styleLogin.login_containerForm,
+  inputContainerClassName: `${styleLogin.login_inputContainer} ${styleForm.form}`,
+  passwordContainerClassName: `${styleLogin.login_passwordContainer}`,
+  buttonClassName: `${styleButton.btn} ${styleButton.btn_primary}`,
+  signUpText: `${styleButton.btn} ${styleButton.btn_secondary} ${styleLogin.login_signUp}`,
+  forgotPasswordClass: styleLogin.login_forgotContainer,
 }
 
 const LoginPage: FC<any> = ({
-	lng,
-	lngDict,
-	brand,
-	hasGoogleAuth,
-	hasFacebookAuth,
+  lng,
+  lngDict,
+  brand,
+  hasGoogleAuth,
+  hasFacebookAuth,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-	const i18n: any = useI18n()
+  const i18n: any = useI18n()
 
-	return (
-		<Layout i18n={i18n} lng={lng} lngDict={lngDict} brand={brand}>
-			<SEO title={i18n.t('login.title')} />
-			<div className={styleLogin.login}>
-				<div className={styleLogin.login_breadcrumb}>
-					<Breadcrumb
-						steps={[{ label: i18n.t('breadcrumb.home') }, { label: i18n.t('breadcrumb.login') }]}
-					/>
-				</div>
-				<div className={styleLogin.login_header}>
-					<h3>{i18n.t('login.title')}</h3>
-				</div>
-				<div className={styleLogin.login_container}>
-					<Login
-						classes={loginClasses}
-						onCompletedMsg={(msg) => toast.success(msg)}
-						onErrorMsg={(msg) => toast.error(msg)}
-						passwordViewIcon={<RiEyeCloseLine />}
-						passwordHideIcon={<RiEye2Line />}
-						loadingComponent={<Loader color="text-light" />}
-					/>
-					{(hasGoogleAuth || hasFacebookAuth) && (
-						<div>
-							<label className={styleLogin.login_separator}>
-								<span>{i18n.t('login.or')}</span>
-							</label>
-							<div className={styleLogin.login_containerAuth}>
-								<SingleSignOn
-									className={styleLogin.login_containerAuth_item}
-									buttonText={`${i18n.t('login.login')} ${i18n.t('login.sso')}`}
-									loadingComponent={
-										<div>
-											<LoaderPages />
-										</div>
-									}
-								/>
-							</div>
-						</div>
-					)}
-				</div>
-			</div>
-		</Layout>
-	)
+  return (
+    <Layout i18n={i18n} lng={lng} lngDict={lngDict} brand={brand}>
+      <SEO title={i18n.t('login.title')} />
+      <div className={styleLogin.login}>
+        <div className={styleLogin.login_breadcrumb}>
+          <Breadcrumb
+            steps={[{ label: i18n.t('breadcrumb.home') }, { label: i18n.t('breadcrumb.login') }]}
+          />
+        </div>
+        <div className={styleLogin.login_header}>
+          <h3>{i18n.t('login.title')}</h3>
+        </div>
+        <div className={styleLogin.login_container}>
+          <Login
+            classes={loginClasses}
+            onCompletedMsg={(msg) => toast.success(msg)}
+            onErrorMsg={(msg) => toast.error(msg)}
+            passwordViewIcon={<RiEyeCloseLine />}
+            passwordHideIcon={<RiEye2Line />}
+            loadingComponent={<Loader color="text-light" />}
+          />
+          {(hasGoogleAuth || hasFacebookAuth) && (
+            <div>
+              <label className={styleLogin.login_separator}>
+                <span>{i18n.t('login.or')}</span>
+              </label>
+              <div className={styleLogin.login_containerAuth}>
+                <SingleSignOn
+                  className={styleLogin.login_containerAuth_item}
+                  buttonText={`${i18n.t('login.login')} ${i18n.t('login.sso')}`}
+                  loadingComponent={
+                    <div>
+                      <LoaderPages />
+                    </div>
+                  }
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res, params }) => {
-	const { default: lngDict = {} } = await import(`locales/${params.lng}.json`)
+  const { default: lngDict = {} } = await import(`locales/${params.lng}.json`)
 
-	const brand = await useBrand(req)
+  const brand = await useBrand(req)
 
-	const cookies = parseCookies(req)
-	const hasGoogleAuth = await useGoogleAuth(req)
-	const hasFacebookAuth = await useFacebookAuth(req)
-	redirectIfAuthenticated(res, cookies, 'account')
+  const cookies = parseCookies(req)
+  const hasGoogleAuth = await useGoogleAuth(req)
+  const hasFacebookAuth = await useFacebookAuth(req)
+  redirectIfAuthenticated(res, cookies, 'account')
 
-	return {
-		props: {
-			lng: params.lng,
-			lngDict,
-			hasGoogleAuth,
-			hasFacebookAuth,
-			brand: brand || '',
-		},
-	}
+  return {
+    props: {
+      lng: params.lng,
+      lngDict,
+      hasGoogleAuth,
+      hasFacebookAuth,
+      brand: brand || '',
+    },
+  }
 }
 
 export default LoginPage

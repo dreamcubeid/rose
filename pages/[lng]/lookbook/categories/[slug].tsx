1321622d944858a8b53ac6eab0703e5d4e913090
jetly <jetly27sandita@gmail.com>
2021-09-21 22:18:56 +0700

    fix: clean code - remove unused code - indentation rule

--- pages/[lng]/lookbook/categories/[slug].tsx
+++ pages/[lng]/lookbook/categories/[slug].tsx
@@ -1,25 +1,25 @@
+/* library package */
 import { FC, useState } from 'react'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import { useRouter } from 'next/router'
-import { isLookbookAllowed, LookbookSingle, useI18n } from '@sirclo/nexus'
+import { 
+  isLookbookAllowed, 
+  LookbookSingle, 
+  useI18n 
+} from '@sirclo/nexus'
+/* library template */
 import { useBrand } from 'lib/useBrand'
 import useWindowSize from 'lib/useWindowSize'
+/* components */
 import Layout from 'components/Layout/Layout'
-import Placeholder from 'components/Placeholder'
-
-import styleLookbook from 'public/scss/pages/Lookbook.module.scss'
 import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
+/* styles */
+import styleLookbook from 'public/scss/pages/Lookbook.module.scss'
 
 const classesLookbookSingle = {
   containerClassName: `w-100`,
   rowClassName: styleLookbook.lookbook_detail,
   imageClassName: 'd-none',
   thumbnailImageClassName: styleLookbook.lookbook_detail_img,
-  // activeClassName: '',
-}
-
-const classesPlaceholderLookbook = {
-  // placeholderList: `${styles.lookbook_placeholder} d-block p-0 mt-0 mb-3 mx-auto w-100`,
 }
 
 const LookbookSinglePage: FC<any> = ({
@@ -29,14 +29,19 @@ const LookbookSinglePage: FC<any> = ({
   brand,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const i18n: any = useI18n()
-  const router = useRouter()
   const size = useWindowSize()
   const LookbookAllowed = isLookbookAllowed()
 
   const [title, setTitle] = useState<string>('')
 
   return (
-    <Layout i18n={i18n} lng={lng} lngDict={lngDict} brand={brand} withAllowed={LookbookAllowed}>
+    <Layout 
+      i18n={i18n} 
+      lng={lng} 
+      lngDict={lngDict} 
+      brand={brand} 
+      withAllowed={LookbookAllowed}
+    >
       <div className={styleLookbook.lookbook_container}>
         <div className={styleLookbook.lookbook_breadcrumb}>
           <Breadcrumb
@@ -62,18 +67,8 @@ const LookbookSinglePage: FC<any> = ({
           classes={classesLookbookSingle}
           slug={slug}
           getTitle={setTitle}
-          loadingComponent={
-            <></>
-            // <div className="mt-3">
-            //   <Placeholder classes={classesPlaceholderLookbook} withList listMany={5} />
-            // </div>
-          }
-          emptyStateComponent={
-            <></>
-            // <p className="d-flex flex-row align-items-center justify-content-center text-align-center p-5">
-            //   {i18n.t('lookbook.isEmpty')}
-            // </p>
-          }
+          loadingComponent={<></>}
+          emptyStateComponent={<></>}
           mode="thumbnail"
           thumborSetting={{
             width: size.width < 768 ? 400 : 600,
@@ -81,14 +76,6 @@ const LookbookSinglePage: FC<any> = ({
             quality: 85,
           }}
         />
-
-        {/* <div
-              className={`${styles.lookbook_nav} d-flex flex-row align-items-center justify-content-between`}
-            >
-              <button onClick={() => router.back()}>{i18n.t('global.back')}</button>
-            </div>
-          </div> */}
-        {/* </div> */}
       </div>
     </Layout>
   )

32e9dc2ad26ef0c3b69472bbb569bdc186de9cd2
jetly <jetly27sandita@gmail.com>
2021-09-22 17:53:04 +0700

    feat: forgot password - new style forgot password

--- pages/[lng]/forgot-password/index.tsx
+++ pages/[lng]/forgot-password/index.tsx
@@ -1,31 +1,32 @@
-import { FC } from "react";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { ResetPassword, useI18n } from "@sirclo/nexus";
-import SEO from "components/SEO";
-import Layout from "components/Layout/Layout";
-import Loader from "components/Loader/Loader";
-import { parseCookies } from "lib/parseCookies";
-import redirectIfAuthenticated from "lib/redirectIfAuthenticated";
-import { useBrand } from "lib/useBrand";
-import { toast } from "react-toastify";
-import styles from "public/scss/pages/Login.module.scss";
+import { FC, useState } from 'react'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { ResetPassword, useI18n } from '@sirclo/nexus'
+import SEO from 'components/SEO'
+import Layout from 'components/Layout/Layout'
+import Loader from 'components/Loader/Loader'
+import { parseCookies } from 'lib/parseCookies'
+import redirectIfAuthenticated from 'lib/redirectIfAuthenticated'
+import { useBrand } from 'lib/useBrand'
+import { toast } from 'react-toastify'
+import styleLogin from 'public/scss/pages/Login.module.scss'
+import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
+import styleForm from 'public/scss/components/Form.module.scss'
+import styleButton from 'public/scss/components/Button.module.scss'
 
 const classesResetPassword = {
-  containerClassName: `${styles.login_item} ${styles.login_item__form}`,
-  inputContainerClassName: styles.sirclo_form_row,
-  inputClassName: `form-control ${styles.sirclo_form_input}`,
-  buttonClassName: `btn 
-    ${styles.btn_primary} ${styles.btn_long} 
-    ${styles.btn_full_width} ${styles.btn_center}`,
-  spinnerClassName: "spinner-border text-light spinner-border-sm",
+  containerClassName: styleLogin.login_containerForm,
+  inputContainerClassName: `${styleLogin.login_inputContainer} ${styleForm.form}`,
+  buttonClassName: `${styleButton.btn} ${styleButton.btn_primary}`,
+  spinnerClassName: 'spinner-border text-light spinner-border-sm',
 }
 
 const ForgotPassword: FC<any> = ({
   lng,
   lngDict,
-  brand
+  brand,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const i18n: any = useI18n();
+  const i18n: any = useI18n()
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
 
   return (
     <Layout
@@ -33,50 +34,61 @@ const ForgotPassword: FC<any> = ({
       lng={lng}
       lngDict={lngDict}
       brand={brand}
+      headerTitle={i18n.t('resetPassword.title')}
     >
-      <SEO title={i18n.t("resetPassword.title")} />
-      <section className={styles.login_wrapper}>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4 d-flex flex-column align-items-start justify-content-start flex-nowrap">
-              <div className={`${styles.login_item} ${styles.login_item__title}`}>
-                <h3>{i18n.t("resetPassword.title")}</h3>
-                <span>{i18n.t("resetPassword.enterEmailBody")}</span>
-              </div>
+      <div className={styleLogin.login}>
+        <div className={styleLogin.login_breadcrumb}>
+          <Breadcrumb
+            steps={[
+              { label: i18n.t('breadcrumb.home') },
+              { label: i18n.t('breadcrumb.forgotPassword') },
+            ]}
+          />
+        </div>
+        {isSuccess ? (
+          <div className={styleLogin.forgotPassword}>
+            <div className={styleLogin.forgotPassword_successTitle}>
+              {i18n.t('forgotPassword.titleSuccessful')}
+            </div>
+            <div className={styleLogin.forgotPassword_successDesc}>
+              {i18n.t('forgotPassword.requestSuccessful')}
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className={styleLogin.login_forgotPasswordDesc}>
+              <span>{i18n.t('resetPassword.enterEmailBody')}</span>
+            </div>
+            <div className={styleLogin.login_container}>
               <ResetPassword
                 classes={classesResetPassword}
                 onErrorMsg={(msg) => toast.error(msg)}
                 loadingComponent={<Loader color="text-light" />}
+                onSuccessMsg={() => setIsSuccess(true)}
               />
             </div>
-          </div>
-        </div>
-      </section>
+          </>
+        )}
+      </div>
     </Layout>
-  );
-};
+  )
+}
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req,
-  res,
-  params
-}) => {
-  const { default: lngDict = {} } = await import(
-    `locales/${params.lng}.json`
-  );
+export const getServerSideProps: GetServerSideProps = async ({ req, res, params }) => {
+  const { default: lngDict = {} } = await import(`locales/${params.lng}.json`)
 
-  const brand = await useBrand(req);
+  const brand = await useBrand(req)
 
-  const cookies = parseCookies(req);
-  redirectIfAuthenticated(res, cookies, 'account');
+  const cookies = parseCookies(req)
+  redirectIfAuthenticated(res, cookies, 'account')
 
   return {
     props: {
       lng: params.lng,
       lngDict,
-      brand: brand || ""
-    }
-  };
+      brand: brand || '',
+    },
+  }
 }
 
-export default ForgotPassword;
+export default ForgotPassword

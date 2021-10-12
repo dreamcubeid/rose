ae19e7fc9e245ae419a3c9c41a927a0ae3edf602
jetly <jetly27sandita@gmail.com>
2021-10-12 18:37:35 +0700

    feat: clean code

--- pages/[lng]/contact/index.tsx
+++ pages/[lng]/contact/index.tsx
@@ -1,45 +1,46 @@
-import { FC } from "react";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import dynamic from "next/dynamic";
-import {
-  useI18n,
-  Contact,
-  Widget,
-  isEnquiryAllowed
-} from "@sirclo/nexus";
-import Layout from "components/Layout/Layout";
-import { useBrand } from "lib/useBrand";
-import { toast } from "react-toastify";
-import styleContact from "public/scss/pages/Contact.module.scss";
-import styleForm from "public/scss/components/Form.module.scss";
-import styleButton from "public/scss/components/Button.module.scss";
-import Breadcrumb from "components/Breadcrumb/Breadcrumb";
+/* library package */
+import { FC } from 'react'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import dynamic from 'next/dynamic'
+import { toast } from 'react-toastify'
+import { 
+  useI18n, 
+  Contact, 
+  Widget, 
+  isEnquiryAllowed 
+} from '@sirclo/nexus'
+/* library template */
+import { useBrand } from 'lib/useBrand'
+/* components */
+import Layout from 'components/Layout/Layout'
+import Breadcrumb from 'components/Breadcrumb/Breadcrumb'
+/* styles */
+import styleContact from 'public/scss/pages/Contact.module.scss'
+import styleForm from 'public/scss/components/Form.module.scss'
+import styleButton from 'public/scss/components/Button.module.scss'
+import stylePlaceholder from 'public/scss/components/Placeholder.module.scss'
 
-const Placeholder = dynamic(() => import("components/Placeholder"));
+const Placeholder = dynamic(() => import('components/Placeholder'))
 
 const classesContact = {
   containerClassName: styleContact.contact_body,
   mapClassName: 'd-none',
   inputContainerClassName: `${styleForm.form} my-4 ${styleContact.contact_form}`,
-  titleClassName: "d-none",
+  titleClassName: 'd-none',
   buttonClassName: `${styleButton.btn} ${styleButton.btn_primary} `,
-  // inputClassName: ,
-  // labelClassName: `d-flex flex-row align-items-center justify-content-start`,
-  // buttonContainerClassName: `${styles.contact_buttonContainer} d-block mt-4`,
-  widgetClassName: styleContact.contact_widget
-};
+  widgetClassName: styleContact.contact_widget,
+}
 
 const classesPlaceholderContact = {
-  // placeholderList: `${styles.placeholderItem} ${styles.placeholderItem_contactWidget}`
+  placeholderList: `${stylePlaceholder.placeholderItem} ${stylePlaceholder.placeholderItem_widgetList}`,
 }
 
 const ContactPage: FC<any> = ({
   lng,
   lngDict,
-  brand
+  brand,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-
-  const i18n: any = useI18n();
+  const i18n: any = useI18n()
   const allowedEnquiry = isEnquiryAllowed()
 
   return (
@@ -49,6 +50,7 @@ const ContactPage: FC<any> = ({
       lngDict={lngDict}
       brand={brand}
       headerTitle={i18n.t('contact.title')}
+      withFooter={false}
       withAllowed={allowedEnquiry}
     >
       <div className={styleContact.contact_container}>
@@ -57,86 +59,39 @@ const ContactPage: FC<any> = ({
             steps={[{ label: i18n.t('breadcrumb.home') }, { label: i18n.t('breadcrumb.contact') }]}
           />
         </div>
+        <div className={styleContact.contact_info}>{i18n.t('contact.info')}</div>
         <Contact
           classes={classesContact}
           isAddressDetail={false}
-          onCompleted={() => toast.success(i18n.t("contact.submitSuccess"))}
-          onError={() => toast.error(i18n.t("contact.submitError"))}
+          onCompleted={() => toast.success(i18n.t('contact.submitSuccess'))}
+          onError={() => toast.error(i18n.t('contact.submitError'))}
           widget={
             <Widget
               pos="footer-4"
-              widgetClassName={styleContact.contact_info}
+              widgetClassName={styleContact.contact_widgetInfo}
               loadingComponent={
-                <Placeholder
-                  classes={classesPlaceholderContact}
-                  withList
-                  listMany={5}
-                />
+                <Placeholder classes={classesPlaceholderContact} withList listMany={5} />
               }
             />
           }
         />
       </div>
-
-      {/* <div className="container">
-        <div className="row">
-          <div className="col-12 col-sm-8 offset-sm2 col-md-6 offset-md-3 col-lg-4 offset-lg-4">
-
-            <div className={`${styles.contact_info} ${styles.contact_info__top}`}>
-              <h1>{i18n.t("contact.title")}</h1>
-              <Widget
-                pos="footer-3"
-                widgetClassName={styles.contact_info}
-                loadingComponent={
-                  <Placeholder
-                    classes={classesPlaceholderContact}
-                    withList
-                    listMany={5}
-                  />
-                }
-              />
-            </div>
-
-            <Contact
-              classes={classesContact}
-              isAddressDetail={false}
-              onCompleted={() => toast.success(i18n.t("contact.submitSuccess"))}
-              onError={() => toast.error(i18n.t("contact.submitError"))}
-              widget={
-                <Widget
-                  pos="footer-4"
-                  widgetClassName={styles.contact_info}
-                  loadingComponent={
-                    <Placeholder
-                      classes={classesPlaceholderContact}
-                      withList
-                      listMany={5}
-                    />
-                  }
-                />
-              }
-            />
-          </div>
-        </div>
-      </div> */}
     </Layout>
-  );
-};
+  )
+}
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  const { default: lngDict = {} } = await import(
-    `locales/${params.lng}.json`
-  );
+  const { default: lngDict = {} } = await import(`locales/${params.lng}.json`)
 
-  const brand = await useBrand(req);
+  const brand = await useBrand(req)
 
   return {
     props: {
       lng: params.lng,
       lngDict,
-      brand: brand || ""
-    }
-  };
+      brand: brand || '',
+    },
+  }
 }
 
-export default ContactPage;
+export default ContactPage
